37e37ddb9d66ad22db547d9c3e88d9cecc9fe426
Antonio <ar0x2ff@gmail.com>
2019-03-27 21:29:04 +0100

    first

---
+++ carica_file.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+if(!isset($_POST["submit"])){
+    $_SESSION['errore_upload'] = "Inserire un file da caricare.";
+    header("location: index.php");
+    exit();
+}
+
+if($_FILES["file_caricato"]["error"] != UPLOAD_ERR_OK){
+    $_SESSION['errore_upload'] = "Errore nel caricamento del file da parte del server. [";
+
+    switch($_FILES["file_caricato"]["error"]){
+        case UPLOAD_ERR_INI_SIZE: {
+            $_SESSION['errore_upload'] .= "Value: 1; The uploaded file exceeds the upload_max_filesize directive in php.ini. ]";
+            break;
+        }
+        case UPLOAD_ERR_FORM_SIZE: {
+            $_SESSION['errore_upload'] .= "Value: 2; The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form. ]";
+            break;
+        }
+        case UPLOAD_ERR_PARTIAL: {
+            $_SESSION['errore_upload'] .= "Value: 3; The uploaded file was only partially uploaded. ]";
+            break;
+        }
+        case UPLOAD_ERR_NO_FILE: {
+            $_SESSION['errore_upload'] .= "Value: 4; No file was uploaded. ]";
+            break;
+        }
+        case UPLOAD_ERR_NO_TMP_DIR: {
+            $_SESSION['errore_upload'] .= "Value: 6; Missing a temporary folder. Introduced in PHP 5.0.3. ]";
+            break;
+        }
+        case UPLOAD_ERR_CANT_WRITE: {
+            $_SESSION['errore_upload'] .= "Value: 7; Failed to write file to disk. Introduced in PHP 5.1.0. ]";
+            break;
+        }
+        case UPLOAD_ERR_EXTENSION: {
+            $_SESSION['errore_upload'] .= "Value: 8; A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help. Introduced in PHP 5.2.0. ]";
+            break;
+        }
+        default: {
+            $_SESSION['errore_upload'] .= "Errore non contemplato. ]";
+            break;
+        }
+    }
+    header("location: index.php");
+    exit();
+}
+
+$percorso_caricamenti = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "file_caricati";
+$percorso_file = $percorso_caricamenti . DIRECTORY_SEPARATOR . $_FILES["file_caricato"]["name"];
+$spostamento_avvenuto = move_uploaded_file($_FILES["file_caricato"]["tmp_name"], $percorso_file);
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Test file PHP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div>
+            File caricato con successo. (Codice errore: 0) <br>
+            Nome: <?=$_FILES['file_caricato']['name']?> <br>
+            Tipo: <?=$_FILES['file_caricato']['type']?> <br>
+            Dimensioni: <?=$_FILES['file_caricato']['size'] ?> byte <br>
+            Percorso temporaneo: <?=$_FILES['file_caricato']['tmp_name']?> <br>
+        </div>
+
+        <?php if ($spostamento_avvenuto): ?>
+            <div>
+                Il file è stato salvato dal server al percorso <?=$percorso_file?>.
+            </div>
+            <div>
+                Contenuto file: <?=file_get_contents($percorso_file);?>
+            </div>
+        <?php else: ?>
+            <div>
+                Errore durante lo spostamento del file al percorso <?=$percorso_file?> del server.
+            </div>
+        <?php endif; ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+$percorso_caricamenti = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "file_caricati";
+$lista_file = array_diff(scandir($percorso_caricamenti), array('.', '..'));
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Test file PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+    <div>
+        <span>
+            <?php
+            if(!empty($_SESSION['errore_upload'])){
+                echo $_SESSION['errore_upload'];
+                unset($_SESSION['errore_upload']);
+            }
+            ?>
+        </span>
+
+        <form action="carica_file.php" method="POST" enctype="multipart/form-data">
+            <label for="campo_file">Inserisci il file da caricare: </label>
+            <input type="file" id="campo_file" name="file_caricato">
+            <input type="submit" name="submit" value="Carica"> 
+        </form>
+    </div>
+
+    <div>
+        <form action="operazioni_file.php" id="form_operazioni">
+            <input type="text" name="nome_file" placeholder="Nome file...">
+            <label for="modalita">Specificare modalità di apertura file</label>
+            <select name="modalita" id="modalita">
+                <option value="r">r</option>
+                <option value="r+">r+</option>
+                <option value="w">w</option>
+                <option value="w">w+</option>
+                <option value="a">a</option>
+                <option value="a+">a+</option>
+            </select>
+            <input type="checkbox" name="binario" value="binario">File binario<br>
+
+            <div>
+                <label for="numero_byte">Numero byte</label>
+                <input type="number" name="numero_byte" id="numero_byte">
+                <input type="submit" name="leggi" value="Leggi">
+            </div>
+
+            <div>
+                <textarea form="form_operazioni" placeholder="Testo..."></textarea>
+                <input type="submit" name="scrivi" value="Scrivi">
+            </div> 
+        </form>
+    </div>
+
+    <div>
+        <ul>
+            <?php foreach ($lista_file as $file): ?>
+            <li> <?=$file?> </li>
+            <?php endforeach; ?> 
+        </ul>
+    </div>
+</body>
+</html>
